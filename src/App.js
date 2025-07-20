ca8c2b7039b253f4e489341bc60549a7ae66fdd1
Tuan Anh Le <anhlt1983@gmail.com>
2025-07-20 17:38:08 +0700

    feat: Create registration form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import 'wired-elements';
 import './App.css';
 
@@ -7,9 +7,24 @@ function App() {
     <div className="App">
       <header className="App-header">
         <wired-card>
-          <h1>Welcome to My Cartoon App!</h1>
-          <p>This is a paragraph inside a wired-card.</p>
-          <wired-button>Click Me!</wired-button>
+          <h1>Registration Form</h1>
+          <div className="form">
+            <wired-input placeholder="First Name"></wired-input>
+            <wired-input placeholder="Last Name"></wired-input>
+            <wired-input type="email" placeholder="Email"></wired-input>
+            <wired-input type="tel" placeholder="Phone Number"></wired-input>
+            <div className="gender-selection">
+              <wired-radio-group selected="male">
+                <wired-radio value="male">Male</wired-radio>
+                <wired-radio value="female">Female</wired-radio>
+                <wired-radio value="other">Other</wired-radio>
+              </wired-radio-group>
+            </div>
+            <div className="buttons">
+              <wired-button>Submit</wired-button>
+              <wired-button>Cancel</wired-button>
+            </div>
+          </div>
         </wired-card>
       </header>
     </div>
