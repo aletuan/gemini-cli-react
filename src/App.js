51232d35f3fbbef550874b9de3e4882d7965f6c6
Tuan Anh Le <anhlt1983@gmail.com>
2025-07-20 17:42:49 +0700

    feat: Add labels and remove border from registration form

--- src/App.js
+++ src/App.js
@@ -6,26 +6,37 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <wired-card>
-          <h1>Registration Form</h1>
-          <div className="form">
+        <h1>Registration Form</h1>
+        <div className="form">
+          <div className="form-field">
+            <label>First Name</label>
             <wired-input placeholder="First Name"></wired-input>
-            <wired-input placeholder="Last Name"></wired-input>
-            <wired-input type="email" placeholder="Email"></wired-input>
-            <wired-input type="tel" placeholder="Phone Number"></wired-input>
-            <div className="gender-selection">
-              <wired-radio-group selected="male">
-                <wired-radio value="male">Male</wired-radio>
-                <wired-radio value="female">Female</wired-radio>
-                <wired-radio value="other">Other</wired-radio>
-              </wired-radio-group>
-            </div>
-            <div className="buttons">
-              <wired-button>Submit</wired-button>
-              <wired-button>Cancel</wired-button>
-            </div>
           </div>
-        </wired-card>
+          <div className="form-field">
+            <label>Last Name</label>
+            <wired-input placeholder="Last Name"></wired-input>
+          </div>
+          <div className="form-field">
+            <label>Email</label>
+            <wired-input type="email" placeholder="Email"></wired-input>
+          </div>
+          <div className="form-field">
+            <label>Phone Number</label>
+            <wired-input type="tel" placeholder="Phone Number"></wired-input>
+          </div>
+          <div className="gender-selection">
+            <label>Gender</label>
+            <wired-radio-group selected="male">
+              <wired-radio value="male">Male</wired-radio>
+              <wired-radio value="female">Female</wired-radio>
+              <wired-radio value="other">Other</wired-radio>
+            </wired-radio-group>
+          </div>
+          <div className="buttons">
+            <wired-button>Submit</wired-button>
+            <wired-button>Cancel</wired-button>
+          </div>
+        </div>
       </header>
     </div>
   );
